cb84b85e182bee8645724120ba9fff15e7b03610
yozhikvtumane <toshamatveeev@gmail.com>
2014-10-02 16:33:34 +0400

    Update slow-scroll.js

--- slow-scroll.js
+++ slow-scroll.js
@@ -1,13 +1,14 @@
 <script src="js/scroll-lp.js"></script>
 	<script>
 			$(document).ready(function(){
+				$('a[data-toggle="newxtstep"]').click(function(){
+					__scroll_vertical__($(this).attr('href'),700)
+			});
 				// $('.header__downbutton_wrap a').click(function(){
 				// 	__scroll_vertical__($(this).attr('href'),700);
-				$('a[data-toggle="newxtstep"]').click(function(){
-					__scroll_vertical__($(this).attr('href'),700)});
+				
 				// $('.downbutton__photos').click(function(){
 				// 	__scroll_vertical__($(this).attr('href'),700);
 				// $('.upbutton').click(function(){
 				// 	__scroll_vertical__($(this).attr('href'),700);
-			});
 	</script>
